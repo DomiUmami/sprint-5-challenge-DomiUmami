c4f6ea4245c301b30a7e6f4e8b42c0aa71f75bde
DomiUmami <williams_dominik@yahoo.com>
2023-11-15 15:50:25 -0500

    Oh my god finaaallly

--- frontend/index.js
+++ frontend/index.js
@@ -24,69 +24,71 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
 
     document.querySelector('p.info').textContent = `No learner is selected`
     
-  
+    
     function buildLearnerCard(learner, mentors){
       const card = document.createElement('div')
       card.classList.add('card')
       card.style.width = '267.81px'
-    
       
-
       if (learner && learner.fullName) {
-       const nameP = document.createElement('h3');
-       nameP.textContent = `${learner.fullName}`
-       nameP.id = `learnerName_${learner.id}`
-       card.appendChild(nameP)
-     }
-    
-    if (learner && learner.email) {
-      const emailP = document.createElement('div');
-      emailP.textContent = `${learner.email}`;
-      card.appendChild(emailP);
-    }
-     
-    if (learner && learner.mentors) {
-      const mentorsP = document.createElement('h4')
-      mentorsP.classList.add('closed')
-      mentorsP.textContent = 'Mentors';
-      
-      mentorsP.addEventListener('click', () => {
-        const mentorsList = card.querySelector('ul');
-        mentorsList.style.display = mentorsList.style.display === 'none' ? 'block' : 'none';
-      
-        document.querySelectorAll('.card.selected').forEach((selectedCard) => {
-          selectedCard.classList.remove('selected');
-        });
-
-        card.classList.add('selected');
-        document.querySelector('p.info').textContent = `The selected learner is ${learner.fullName}`;
-      
-        mentorsP.classList.toggle('closed');
-        mentorsP.classList.toggle('open');
-      });
-  
-      const matchingMentors = mentors.filter(mentor => learner.mentors.includes(mentor.id));
-  
-      const mentorsList = document.createElement('ul');
-      mentorsList.style.display = 'none'
-      
-      mentorsP.appendChild(mentorsList)
-      
-  
-      if(matchingMentors.length > 0){
-        matchingMentors.forEach(mentor => {
-          const mentorsName = document.createElement('li')
-          mentorsName.textContent = `${mentor.firstName} ${mentor.lastName}`
-          mentorsList.appendChild(mentorsName)
-        })
-      } else {
-        console.log("Mentor not found")
+        const nameP = document.createElement('h3');
+        nameP.textContent = `${learner.fullName}`
+        nameP.id = `learnerName_${learner.id}`
+        card.appendChild(nameP)
       }
+      
+      if (learner && learner.email) {
+        const emailP = document.createElement('div');
+        emailP.textContent = `${learner.email}`;
+        card.appendChild(emailP);
+      }
+      
+      if (learner && learner.mentors) {
+        const mentorsP = document.createElement('h4')
+        mentorsP.textContent = 'Mentors'
+        mentorsP.classList.add('closed')
+        
+        mentorsP.addEventListener('click', () => {
+          const mentorsList = mentorsP.nextElementSibling;
+          mentorsList.style.display = mentorsList.style.display === 'none' ? 'block' : 'none';
+          
+          document.querySelectorAll('.card.selected').forEach((selectedCard) => {
+            selectedCard.classList.remove('selected');
+          });
+          
+          card.classList.add('selected');
+          document.querySelector('p.info').textContent = `The selected learner is ${learner.fullName}`;
+          
+          mentorsP.classList.toggle('closed');
+          mentorsP.classList.toggle('open');
+          
+        });
+        
+        const matchingMentors = mentors.filter(mentor => learner.mentors.includes(mentor.id));
+        
+        const mentorsList = document.createElement('ul');
+        mentorsList.style.display = 'none'
+        
+        
+        
+        if(matchingMentors.length > 0){
+          
+          
+          matchingMentors.forEach(mentor => {
+            const mentorsName = document.createElement('li')
+            mentorsName.textContent = `${mentor.firstName} ${mentor.lastName}`
+            mentorsList.appendChild(mentorsName)
+          })
+        } else {
+          console.log("Mentor not found")
+        }
         card.appendChild(mentorsP)
-    }
-    
-    const originalNameContent = learner.fullName
-
+        card.appendChild(mentorsList)
+      }
+      
+      
+      const originalNameContent = learner.fullName
+      
     card.addEventListener('click', (evt) => {
       const isMentorsListClick = evt.target.closest('h4') !== null;
       
@@ -96,8 +98,6 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
           selectedCard.classList.remove('selected');
           selectedCard.querySelector('h3').textContent = selectedCard.originalNameContent
           
-          
-        
         })
 
         if (!isSelected) {
